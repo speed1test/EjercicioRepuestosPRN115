6f01ec9624c26df2ea65b1e7b065c0a474cb3734
Lopez Astro <edwin12sv@gmail.com>
2022-08-28 18:04:50 -0600

    first commit

---
+++ Ejercicio2Refuerzo2UES/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Ejercicio2Refuerzo2UES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRepuestos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRepuestos
+            // 
+            this.btnRepuestos.Location = new System.Drawing.Point(61, 51);
+            this.btnRepuestos.Name = "btnRepuestos";
+            this.btnRepuestos.Size = new System.Drawing.Size(168, 76);
+            this.btnRepuestos.TabIndex = 0;
+            this.btnRepuestos.Text = "Repuestos";
+            this.btnRepuestos.UseVisualStyleBackColor = true;
+            this.btnRepuestos.Click += new System.EventHandler(this.btnRepuestos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 186);
+            this.Controls.Add(this.btnRepuestos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRepuestos;
+    }
+}
+
---
+++ Ejercicio2Refuerzo2UES/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2Refuerzo2UES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRepuestos_Click(object sender, EventArgs e)
+        {
+            bool estado = true;
+            String[] descripcion = new String[100000000];
+            double manoDeObra = 0;
+            double[] precioRepuesto = new double[100000000];
+            double sumatoriaRepuestos = 0;
+            double montoTotal = 0;
+            int contador = 0;
+            int fin = 2;
+            string listaRepuestos = String.Empty;
+
+            do 
+            {
+                descripcion[contador] = Microsoft.VisualBasic.Interaction.InputBox("Ingrese una descripción: ");
+                precioRepuesto[contador] = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Ingrese el valor del repuesto: "));
+                fin = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Desea ingresar más datos? \n 1. Si \n 2. No  "));
+                if (fin == 2) 
+                {
+                    estado = false;
+                }
+                contador++;
+
+            }
+            while (estado);
+            manoDeObra = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Ingrese $ de mano obra:  "));
+            for (int i = 0; i < contador; i++) 
+            {
+                sumatoriaRepuestos += precioRepuesto[i];
+                listaRepuestos += "Nombre del repuesto: "+descripcion[i]+ " \n"+ "Precio de repuesto: " + precioRepuesto[i] + "\n";
+            }
+            montoTotal = (sumatoriaRepuestos + manoDeObra) * 0.13 + (sumatoriaRepuestos + manoDeObra);
+            MessageBox.Show(listaRepuestos+ "\n"+"Monto total a pagar: "+montoTotal);
+        }
+    }
+}
